e29d81de6a7755cbf071b8a209f290d8f48a11a7
Svitlyk M <svitly4ka@ukr.net>
2017-01-23 20:09:49 +0000

    Added map and styles

---
+++ googleMap.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 49.840275, lng: 24.029762};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
