ab3777e6d7ee7af3545a242a26daf10417f2bbf6
Svitlyk M <svitly4ka@ukr.net>
2017-02-02 18:21:38 +0000

    Changed main nav on mobile

--- script/menus.js
+++ script/menus.js
@@ -14,22 +14,39 @@ function initEvent() {
   /** @type {Element} */ var menu = document.getElementById('wraper');
   /** @type {Element} */ var burgerMenu = document.getElementById('burger-button');
   /** @type {Element} */ var btn = document.getElementById('toggle');
+  var body = document.getElementsByTagName('body')[0];
   btn.addEventListener('click', function() {
     toggle(menu);
   });
   burgerMenu.addEventListener('click', function() {
     toggle(tabList);
+    body.classList.toggle('stop-scroll');
   });
 }
 
 function hideMenu() {
   var screenWidth = screen.width;
-  console.log(screen);
   /** @type {Element} */ var burgerMenu = document.getElementById('burger-button');
   /** @type {Element} */ var tabList = document.getElementById('tab-list');
+  /** @type {Element} */ var logo = tabList.childNodes[5];
   if (screenWidth < 768) {
     toggle(burgerMenu);
     toggle(tabList);
+    toggle(logo);
+  }
+}
+function disable_scroll() {
+  document.ontouchmove = function(e) { 
+    e.preventDefault(); 
+  }
+}
+
+function enable_scroll() {
+  /** @type {Element} */ var burgerMenu = document.getElementById('burger-button');
+  if (burgerMenu.classList.contains('hidden')) {
+    document.ontouchmove = function(e) { 
+      return true;
+    }
   }
 }
 
