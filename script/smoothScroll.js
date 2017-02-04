f5d3f88d43fcf493c1c361a7e8d9d5ddb31b3d8c
Svitlyk M <svitly4ka@ukr.net>
2017-02-04 19:01:28 +0000

    Added smooth scrolling

---
+++ script/smoothScroll.js
@@ -0,0 +1,60 @@
+(function() {
+  /** @type {Element} */ var body = document.body;
+  /** @type {HTMLCollection} */
+      var buttons = document.getElementsByClassName('btn');
+  /** @type {number} */ var length = buttons.length;
+  /** @type {number} */ var animateTime = 900;
+  /** @type {Element} */ var button;
+  /** @type {number} */ var i;
+  /** @type {number} */ var targetOffset;
+  /** @type {number} */ var currentPosition;
+  /** @type {string} */ var translate;
+  
+  function getPageScroll() {
+    /** @type {number} */ var yScroll;
+  
+    if (window.pageYOffset) {
+      yScroll = window.pageYOffset;
+    } else if (document.documentElement && document.documentElement.scrollTop) {
+      yScroll = document.documentElement.scrollTop;
+    } else if (document.body) {
+      yScroll = document.body.scrollTop;
+    }
+    return yScroll;
+  }
+  
+  
+  
+  for (i = 0; i < length; i++) {
+    button = buttons[i];
+    button.addEventListener('click', function (event) {
+    
+      targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+      currentPosition = getPageScroll();
+      body.classList.add('in-transition');
+      
+      
+      
+      if (targetOffset > currentPosition) {
+        translate = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+      } else {
+        translate = "translate(0, " + (currentPosition - targetOffset) + "px)";
+      }
+      
+      body.style.WebkitTransform = translate;
+      body.style.MozTransform = translate;
+      body.style.transform = translate;
+      
+      window.setTimeout(function () {
+        body.classList.remove('in-transition');
+        body.style.cssText = "";
+        window.location.hash = event.target.hash;
+      }, animateTime);
+    
+      event.preventDefault();
+    
+    }, false);
+  
+  }
+
+})();
